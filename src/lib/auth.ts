4a33b8c5342e92e1572d09dba0fcd115c214641c
Husain Badri <husainbadri24@gmail.com>
2026-08-19 15:52:47 +0530

    feat: add columbuscleanindia@gmail.com to authorized login emails list

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ export const ALLOWED_EMAILS = [
   'columbuscleanindia.outgrow@gmail.com',
   'sj@columbus-clean.in',
   'info@columbus-clean.in',
+  'columbuscleanindia@gmail.com',
 ]
 
 export function isAuthorizedEmail(email: string | null | undefined) {
